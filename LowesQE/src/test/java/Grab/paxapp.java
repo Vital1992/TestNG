1e89b889711e1f569830c71d105709dcd656af9d
Vitaly Cherkasov <ftt.vitaly.cherkasov@grabtaxi.com>
2019-08-15 12:03:16 -0700

    Added Grab project

--- LowesQE/src/test/java/Grab/paxapp.java
+++ LowesQE/src/test/java/Grab/paxapp.java
@@ -1,4 +1,4 @@
-package lowes;
+package Grab;
 
 import org.testng.annotations.Test;
 import java.net.MalformedURLException;
@@ -16,7 +16,8 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-public class TestAndroidApp {
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+public class paxapp {
   public static URL url;
   public static DesiredCapabilities capabilities;
   public static AndroidDriver<MobileElement> driver;
@@ -31,9 +32,13 @@ public class TestAndroidApp {
     capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
     capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0");
     capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
-    capabilities.setCapability(MobileCapabilityType.APP, "/Users/vitaly.cherkasov/ContactManager.apk");
+    capabilities.setCapability(MobileCapabilityType.APP, "/Users/vitaly.cherkasov/Downloads/debug-5.59.04518978-universal-50590000-4518978.apk");
     capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
     capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+    capabilities.setCapability(AndroidMobileCapabilityType.APP_WAIT_ACTIVITY, "com.grab.pax.newface.presentation.newface.NewFace");
+    capabilities.setCapability(AndroidMobileCapabilityType.APP_WAIT_ACTIVITY, "com.grabtaxi.passenger.com.grab.pax.newface.presentation.newface.NewFace");
+    capabilities.setCapability(AndroidMobileCapabilityType.APP_WAIT_ACTIVITY, "com.grab.on_boarding.ui.OnBoardingActivity");
+    capabilities.setCapability("appPackage", "com.grabtaxi.passenger");
 
     driver = new AndroidDriver<MobileElement>(url, capabilities);
     driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
@@ -42,12 +47,18 @@ public class TestAndroidApp {
 
   @Test (enabled=true) public void addcontact() throws InterruptedException {
 	  driver.resetApp();
-	  //WebDriverWait wait = new WebDriverWait (driver, 10);
-	  //wait.until (ExpectedConditions.visibilityOfElementLocated (By.id ("Add Contact")));
+	  WebDriverWait wait = new WebDriverWait (driver, 10);
+	  wait.until (ExpectedConditions.visibilityOfElementLocated (By.id ("android:id/button1")));
+	  driver.findElement(By.id("android:id/button1")).click();
+	  driver.findElement(By.id("com.android.packageinstaller:id/permission_allow_button")).click();
+	  driver.findElement(By.id("com.grabtaxi.passenger:id/on_boarding_google_login_button")).click();
+
+	 /*
 	  driver.findElementByAccessibilityId(Locators.AddContact).click();	
 	  driver.findElement(By.id(Locators.ContactNameEdit)).sendKeys("test");	
 	  driver.findElement(By.id(Locators.ContactEmailEdit)).sendKeys("test@gmail.com");
 	  driver.findElementByAccessibilityId(Locators.Save).click();
+	  */
 	  driver.quit();
   }
 }
